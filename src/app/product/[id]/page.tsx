1ab196465bffa4d97cfbd9904686c6459248ff0a
abdrhxyii <abdrhxyii@git>
2024-08-14 21:50:30 +0530

    product detail page added

---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { useParams } from 'next/navigation';
+import { useState } from 'react';
+import Image from 'next/image';
+import Navbar from '@/app/Components/Navbar';
+
+const images = [
+  '/product.png',
+  '/product.png',
+  '/product.png',
+  '/product.png',
+  '/product.png',
+];
+
+export default function product() {
+  const [mainImage, setMainImage] = useState(images[0]);
+
+  return (
+    <div>
+    <Navbar/>
+    <div className="container mx-auto px-4 py-8">
+      <div className="flex flex-col md:flex-row">
+        {/* Image Section */}
+        <div className="md:w-1/2">
+          <div className="mb-4">
+            <Image src={mainImage} alt="Watch" width={500} height={500} className="w-full" />
+          </div>
+          <div className="flex space-x-2">
+            {images.map((img, index) => (
+              <button key={index} onClick={() => setMainImage(img)} className="w-1/5">
+                <Image src={img} alt={`Watch ${index + 1}`} width={100} height={100} className="w-full" />
+              </button>
+            ))}
+          </div>
+        </div>
+
+        {/* Product Details Section */}
+        <div className="md:w-1/2 md:pl-8 mt-8 md:mt-0">
+          <h1 className="text-3xl font-bold mb-4">Royal Oak Mini Frosted Gold Quartz</h1>
+          <p className="text-gray-600 mb-4">
+            Brighten up your wardrobe with our Women's A-Line Yellow Dress. This vibrant and trendy dress features a stunning A-line silhouette that cinches at the waist and flows out gracefully, creating a chic and elegant look. The sunny yellow hue adds a pop of color, perfect for summer outings or any event.
+          </p>
+          <p className="text-2xl font-bold mb-4">LKR 23,000</p>
+          <button className="bg-black text-white py-2 px-4 w-full mb-2">Add to cart</button>
+          <button className="bg-gray-200 text-black py-2 px-4 w-full">Buy now</button>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+}
