c43a8d0d7646ee4d8137974f99686697fa396cb3
abdrhxyii <abdrhxyii@git>
2024-08-20 19:51:43 +0530

    cart ui updated

--- src/app/Components/Cart.tsx
+++ src/app/Components/Cart.tsx
@@ -6,37 +6,37 @@ export default function Cart ({ onClose }: any) {
   return (
     <div className="fixed top-0 right-0 h-full w-full sm:w-[400px] bg-white shadow-lg z-50">
       <div className="flex justify-between items-center p-4 border-b">
-        <h2 className="text-xl font-semibold">Your Cart</h2>
-        <X className="cursor-pointer" onClick={onClose} />
+        <h2 className="text-xl font-semibold text-black">Your Cart</h2>
+        <X className="cursor-pointer text-black" onClick={onClose} />
       </div>
       <div className="p-4 space-y-4 overflow-y-auto">
-        {Array.from({ length: 3 }).map((_, idx) => (
+        {Array.from({ length: 15 }).map((_, idx) => (
           <div
             key={idx}
-            className="flex items-center p-4 bg-gray-100 rounded-lg shadow"
+            className="flex items-center p-2 rounded-lg border border-border-grey"
           >
-            <Image src="/product.png" alt="Product Image" width={60} height={60} />
+            <Image src="/product.png" alt="Product Image" width={70} height={80} className='rounded-lg' />
             <div className="ml-4 flex-1">
-              <h3 className="font-semibold">Royal Oak Mini Frosted Gold Quartz</h3>
+              <h3 className="font-semibold text-black">Royal Oak Mini Frosted Gold Quartz</h3>
               <p className="text-sm text-gray-500">Audemars Piguet</p>
-              <p className="text-sm font-semibold">1 * LKR 20,000</p>
+              <p className="text-sm font-semibold text-black">1 * LKR 20,000</p>
             </div>
             <Trash2 className="cursor-pointer" />
           </div>
         ))}
       </div>
-      <div className="p-4 space-y-2">
+      <div className="p-4 space-y-2 fixed bottom-0 w-full">
         <div className="flex justify-between text-lg font-semibold">
-          <span>Sub Total:</span>
-          <span>LKR 23,000</span>
+          <span className='text-black'>Sub Total:</span>
+          <span className='text-black'>LKR 23,000</span>
         </div>
         <div className="flex justify-between text-lg font-semibold">
-          <span>Sales Discount:</span>
-          <span>LKR 23,000</span>
+          <span className='text-black'>Sales Discount:</span>
+          <span className='text-black'>LKR 23,000</span>
         </div>
         <div className="flex justify-between text-lg font-semibold">
-          <span>Total:</span>
-          <span>LKR 23,000</span>
+          <span className='text-black'>Total:</span>
+          <span className='text-black'>LKR 23,000</span>
         </div>
         <button className="w-full py-2 mt-4 bg-green-500 text-white rounded-lg">
           Checkout
